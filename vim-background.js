9413d402f9e0c16fa7d3ff65c0d587d9c87d51e9
Jonathan Hao <johnathan79717@gmail.com>
2016-08-07 17:04:30 +0800

    Refactor switch tabs with promise generators

--- vim-background.js
+++ vim-background.js
@@ -1,26 +1,3 @@
-function swtich_tab(offset) {
-  // first, get currently active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    if (tabs.length) {
-      var activeTab = tabs[0],
-      tabId = activeTab.id,
-      currentIndex = activeTab.index;
-      // next, get number of tabs in the window, in order to allow cyclic next
-      chrome.tabs.query({currentWindow: true}, function (tabs) {
-        var numTabs = tabs.length;
-        // finally, get the index of the tab to activate and activate it
-        chrome.tabs.query({index: (currentIndex+offset+numTabs) % numTabs}, function(tabs){
-          if (tabs.length) {
-            var tabToActivate = tabs[0],
-            tabToActivate_Id = tabToActivate.id;
-            chrome.tabs.update(tabToActivate_Id, {active: true});
-          }
-        });
-      });
-    }
-  });
-}
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(request);
@@ -28,11 +5,11 @@ chrome.runtime.onMessage.addListener(
     switch (request.type) {
       case "switch_tab_left":
         console.log("Switching to left tab");
-        swtich_tab(-1);
+        switchTab(-1);
         break;
       case "switch_tab_right":
         console.log("Switching to right tab");
-        swtich_tab(1);
+        switchTab(1);
         break;
       case "reload":
         console.log("Reload, bypassCache = " + request.bypassCache);
@@ -80,6 +57,22 @@ function runPromiseGenerator(generator){
     });
   }
   go(gen.next());
+}
+
+function switchTab(offset) {
+  runPromiseGenerator(function *() {
+    // first, get currently active tab
+    let activeTab = yield getActiveTab(),
+        currentIndex = activeTab.index;
+
+    // next, get number of tabs in the window, in order to allow cyclic next
+    let tabs = yield queryTabAll({ currentWindow: true }),
+        numTabs = tabs.length;
+
+    // finally, get the index of the tab to activate and activate it
+    let tabToActivate = yield getTabByIndex((currentIndex+offset+numTabs) % numTabs);
+    yield activateTab(tabToActivate);
+  });
 }
 
 function closeTab(focusLeft) {
