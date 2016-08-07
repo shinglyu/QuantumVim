aa95add6a014aa7fcd9931dbf858b992afbf3ae8
Jonathan Hao <johnathan79717@gmail.com>
2016-08-07 11:19:44 +0800

    d/D closes tabs. fixes #28

--- vim-background.js
+++ vim-background.js
@@ -23,6 +23,7 @@ function swtich_tab(offset) {
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+    console.log(request);
     console.log(sender.tab);
     switch (request.type) {
       case "switch_tab_left":
@@ -37,6 +38,57 @@ chrome.runtime.onMessage.addListener(
         console.log("Reload, bypassCache = " + request.bypassCache);
         chrome.tabs.reload({ bypassCache: request.bypassCache });
         break;
+      case "close_tab":
+        closeTab(request.focusLeft);
+        break;
     }
   }
 );
+
+function queryTabAll(options) {
+  return new Promise(function(resolve) {
+    chrome.tabs.query(options, resolve);
+  });
+}
+
+function queryTab(options) {
+  return queryTabAll(options).then(function (tabs) {
+    return tabs[0];
+  });
+}
+
+function getActiveTab() {
+  return queryTab({ active: true, currentWindow: true });
+}
+
+function getTabByIndex(index) {
+  return queryTab({ index });
+}
+
+function activateTab(tab) {
+  return new Promise(function(resolve) {
+    chrome.tabs.update(tab.id, {active: true}, resolve);
+  });
+}
+
+function runPromiseGenerator(generator){
+  var gen = generator();
+  function go(result){
+    if(result.done) return;
+    result.value.then(function(r){
+      go(gen.next(r));
+    });
+  }
+  go(gen.next());
+}
+
+function closeTab(focusLeft) {
+  runPromiseGenerator(function *() {
+    let tab = yield getActiveTab();
+    if (focusLeft && tab.index > 0) {
+      let tabLeft = yield getTabByIndex(tab.index - 1);
+      yield activateTab(tabLeft);
+    }
+    chrome.tabs.remove(tab.id);
+  });
+}
