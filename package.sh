4a2b4cf40b4e9838f0a10bf90bbb978c4bb857df
Shing Lyu <shing.lyu@gmail.com>
2016-10-02 14:50:36 +0800

    Added focusin lib to package script

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir output
 
-zip output/quantumvim.zip vim.js vim-background.js icons/icon-48.png manifest.json
+zip output/quantumvim.zip vim.js vim-background.js icons/icon-48.png manifest.json focusin.min.js
 
 cd jetpack
 jpm xpi 
